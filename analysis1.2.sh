f27ae086399c333d18321eeec584c5abeb17e04b
hongjiezhao20 <3334192957@qq.com>
2025-05-17 23:03:34 +0800

    Update analysis1.2.sh

--- analysis1.2.sh
+++ analysis1.2.sh
@@ -25,7 +25,7 @@ while :; do
   case $option0 in
     0)
       wget  https://raw.githubusercontent.com/hongjiezhao20/-/refs/heads/main/ananlysisdocument1.2.zip -O adz1.2.zip
-      unzip model.zip -d "$dir"
+      unzip adz1.2.zip -d "$dir"
       echo "分析文件已下载并解压至 $dir"
       ;;
     1)
